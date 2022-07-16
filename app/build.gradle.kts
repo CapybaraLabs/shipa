b9dc556a33d0ee491a3ae11bad2ad1f6d5e80b56
Napster <napster@npstr.space>
2022-07-16 23:23:49 +0200

    Add editorconfig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,30 +8,30 @@
  */
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.7.0"
-    id("com.github.ben-manes.versions") version "0.42.0"
-    id ("org.springframework.boot") version "2.7.1"
+	// Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+	id("org.jetbrains.kotlin.jvm") version "1.7.0"
+	id("com.github.ben-manes.versions") version "0.42.0"
+	id("org.springframework.boot") version "2.7.1"
 
 
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+	// Apply the application plugin to add support for building a CLI application in Java.
+	application
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+	// Use Maven Central for resolving dependencies.
+	mavenCentral()
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.7.0"))
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.7.1"))
+	implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.7.0"))
+	implementation(platform("org.springframework.boot:spring-boot-dependencies:2.7.1"))
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter")
 }
 
 application {
-    // Define the main class for the application.
-    mainClass.set("dev.capybaralabs.shipa.LauncherKt")
+	// Define the main class for the application.
+	mainClass.set("dev.capybaralabs.shipa.LauncherKt")
 }
