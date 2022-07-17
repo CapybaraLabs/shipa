df586bd3bb6a9f2d7fef1f925081d001b41354de
Napster <napster@npstr.space>
2022-07-17 19:30:38 +0200

    Set up config files

--- src/main/kotlin/dev/capybaralabs/shipa/Launcher.kt
+++ src/main/kotlin/dev/capybaralabs/shipa/Launcher.kt
@@ -1,16 +1,19 @@
 package dev.capybaralabs.shipa
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 open class Launcher {
 
-    init {
-    	println("Henlo")
-    }
+	init {
+		println("Henlo")
+	}
 }
 
 fun main(args: Array<String>) {
-	runApplication<Launcher>(*args)
+	System.setProperty("spring.config.name", "shipa")
+	val app = SpringApplication(Launcher::class.java)
+	app.setAdditionalProfiles("secrets")
+	app.run(*args)
 }
