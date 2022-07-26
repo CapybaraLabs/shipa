2a0c498e09b6558dfe5ecf50c430806ff64ff5f8
Napster <napster@npstr.space>
2022-07-26 21:42:48 +0200

    Wtf Kotlin

--- src/main/kotlin/dev/capybaralabs/shipa/discord/interaction/model/ApplicationCommandData.kt
+++ src/main/kotlin/dev/capybaralabs/shipa/discord/interaction/model/ApplicationCommandData.kt
@@ -3,8 +3,7 @@ package dev.capybaralabs.shipa.discord.interaction.model
 /**
  * [Discord Application Command Data](https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-application-command-data-structure)
  */
-@Suppress("RedundantModalityModifier") // Jackson subtype detection of sealed class won't work without it
-final data class ApplicationCommandData(
+data class ApplicationCommandData(
 	val id: Long,
 	val name: String,
 	val type: ApplicationCommandType,
