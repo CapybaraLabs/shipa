5128ac75d845a85619e5e2515f9ae91754ffaff4
Napster <napster@npstr.space>
2022-08-07 19:50:07 +0200

    Embed defaults

--- src/main/kotlin/dev/capybaralabs/shipa/discord/model/Embed.kt
+++ src/main/kotlin/dev/capybaralabs/shipa/discord/model/Embed.kt
@@ -6,62 +6,62 @@ import java.time.Instant
  * [Discord Embed](https://discord.com/developers/docs/resources/channel#embed-object)
  */
 data class Embed(
-	val title: String?,
-	val type: String?,
-	val description: String?,
-	val url: String?,
-	val timestamp: Instant?,
-	val color: Int?,
-	val footer: EmbedFooter?,
-	val image: EmbedImage?,
-	val thumbnail: EmbedThumbnail?,
-	val video: EmbedVideo?,
-	val provider: EmbedProvider?,
-	val author: EmbedAuthor?,
-	val fields: List<EmbedField>?,
+	val title: String? = null,
+	val type: String? = null,
+	val description: String? = null,
+	val url: String? = null,
+	val timestamp: Instant? = null,
+	val color: Int? = null,
+	val footer: EmbedFooter? = null,
+	val image: EmbedImage? = null,
+	val thumbnail: EmbedThumbnail? = null,
+	val video: EmbedVideo? = null,
+	val provider: EmbedProvider? = null,
+	val author: EmbedAuthor? = null,
+	val fields: List<EmbedField>? = null,
 )
 
 data class EmbedFooter(
 	val text: String,
-	val iconUrl: String?,
-	val proxyIconUrl: String?,
+	val iconUrl: String? = null,
+	val proxyIconUrl: String? = null,
 )
 
 data class EmbedImage(
 	val url: String,
-	val proxyUrl: String?,
-	val height: Int?,
-	val width: Int?,
+	val proxyUrl: String? = null,
+	val height: Int? = null,
+	val width: Int? = null,
 )
 
 data class EmbedThumbnail(
 	val url: String,
-	val proxyUrl: String?,
-	val height: Int?,
-	val width: Int?,
+	val proxyUrl: String? = null,
+	val height: Int? = null,
+	val width: Int? = null,
 )
 
 data class EmbedVideo(
-	val url: String?,
-	val proxyUrl: String?,
-	val height: Int?,
-	val width: Int?,
+	val url: String? = null,
+	val proxyUrl: String? = null,
+	val height: Int? = null,
+	val width: Int? = null,
 )
 
 data class EmbedProvider(
-	val name: String?,
-	val url: String?,
+	val name: String? = null,
+	val url: String? = null,
 )
 
 data class EmbedAuthor(
 	val name: String,
-	val url: String?,
-	val iconUrl: String?,
-	val proxyIconUrl: String?,
+	val url: String? = null,
+	val iconUrl: String? = null,
+	val proxyIconUrl: String? = null,
 )
 
 data class EmbedField(
 	val name: String,
 	val value: String,
-	val inline: Boolean?,
+	val inline: Boolean? = null,
 )
