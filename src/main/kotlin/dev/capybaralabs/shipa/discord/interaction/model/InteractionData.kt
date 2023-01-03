cb43e6a14d77e4a67f97de56a0524f4bdcc20d17
Napster <napster@npstr.space>
2023-01-03 18:29:09 +0100

    Data option can be boolean

--- src/main/kotlin/dev/capybaralabs/shipa/discord/interaction/model/InteractionData.kt
+++ src/main/kotlin/dev/capybaralabs/shipa/discord/interaction/model/InteractionData.kt
@@ -25,12 +25,12 @@ sealed interface InteractionData {
 	) : InteractionData {
 
 		/**
-		 * [Discord Application Command Interaction Data Option](https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-interaction-data-option-structure)
+		 * [Discord Application Command Interaction Data Option](https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-application-command-interaction-data-option-structure)
 		 */
 		data class ApplicationCommandInteractionDataOption(
 			val name: String,
 			val type: ApplicationCommandOptionType,
-			val value: String?, // or Int, or Double
+			val value: String?, // or Int, or Double, or Boolean
 			val options: List<ApplicationCommandInteractionDataOption>?,
 			val focused: Boolean?,
 		)
